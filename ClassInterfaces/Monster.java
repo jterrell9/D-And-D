b9670ff8810bbb1ec09035a360ebbf9e96347a5f
Jack Terrell <trifeowns@gmail.com>
2017-02-23 11:03:48 -0500

    changed implements to post conditions

--- ClassInterfaces/Monster.java
+++ ClassInterfaces/Monster.java
@@ -3,32 +3,29 @@ public abstract class Monster{
     private boolean isAlive;
     private boolean isInFight;
 
-    public abstract void attack(Player p1); // basic attack method which all monsters will use
+    public abstract void attack(Player player); // basic attack method which all monsters will use
     public abstract void die(); // basic die method which all monsters will use
     public abstract void takeDamage(int damage); // reduces health when a monster takes damage
     
     // returns stats
     public Stats getStats(){
-        return stats;
     }
 
     // returns if the monster is alive
     public boolean isAlive(){
-        return isAlive;
     }
 
     //returns if the monster is in a fight
     public boolean isInFight(){
-        return isInFight;
     }
     
     //sets the stats
+    //@post: this.stats = stats;
     public void setStats(Stats stats){
-        this.stats = stats;
     }
 
     //sets the variable fight
+    //@post: this.isInFight = fight;
     public void setFight(boolean fight){
-        this.isInFight = fight;
     }
 }
