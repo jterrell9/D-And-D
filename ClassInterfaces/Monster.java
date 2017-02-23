71c59315ca1d180afd8f240d423d454e6a5cf8a9
meracoid <jcreech3@student.gsu.edu>
2017-02-22 22:24:22 -0500

    Added more to Monster class

--- ClassInterfaces/Monster.java
+++ ClassInterfaces/Monster.java
@@ -7,5 +7,9 @@ public interface Monster{
     public void die(); // basic die method which all monsters will use
     public void takeDamage(int damage); // reduces health when a monster takes damage
     public Stats getStats(); // returns the stats of the Monster
-
+    public boolean isAlive(); // returns if the monster is alive
+    public boolean isFight(); // returns if the monser is in a fight
+    public void setStats(Stats stats); // sets the mosnters stats
+    public void setAlive(boolean alive); // sets if the monser is alive
+    public void setFight(boolean fight); // sets if the monser is in a fight
 }
