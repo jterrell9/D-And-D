78864db46fb56958bf4d8828eb9ca80ec0812c9b
Jack Terrell <trifeowns@gmail.com>
2017-03-22 04:07:55 -0400

    added back javaFX, and kept tester.

--- src/com/dd/GameRunner.java
+++ src/com/dd/GameRunner.java
@@ -15,7 +15,7 @@ import javafx.application.Application;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class GameRunner {//extends Application {
+public class GameRunner extends Application {
     private static Stage stage;
     private static List<GameState> gameStateList = new ArrayList<GameState>();
     private static GameState activeGameState;
@@ -25,11 +25,9 @@ public class GameRunner {//extends Application {
 
     public static void main(String[] args) throws FileNotFoundException {
         Tester.go();
-    	//launch(args);
-    	
-    	
+    	launch(args);
     }
-/*
+
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
@@ -70,7 +68,6 @@ public class GameRunner {//extends Application {
         setActiveGameScene("MainMenuScene", null);
         primaryStage.show();
     }
-    */
 
     private static void addGameScene(String name, GameScene gameScene) {
         if(gameSceneMap.containsKey(name))
