6876fe352ffe8d7f38c899690b5de3e3b8918dda
Jack Terrell <trifeowns@gmail.com>
2017-03-22 03:44:21 -0400

    style change

--- src/com/dd/entities/Monster.java
+++ src/com/dd/entities/Monster.java
@@ -2,7 +2,7 @@ package com.dd.entities;
 
 import com.dd.Stats;
 
-public class Monster extends Entity{
+public class Monster extends Entity {
 	
 	public Monster(String name, int health, int attack, int defense) {
 		super(name, health, health, attack, defense);
