dbb437fefaf878eb3fce0fe62fd2ee99d1731ea9
Jack Terrell <trifeowns@gmail.com>
2017-03-22 03:50:25 -0400

    Style conform

--- src/com/dd/levels/DungeonMap.java
+++ src/com/dd/levels/DungeonMap.java
@@ -6,71 +6,77 @@ import com.dd.tester.DIR;
 
 public class DungeonMap {
 
-	public Room[][] rooms;
+	private Room[][] rooms;
 	private int maxRow;
 	private int maxCol;
-	public DungeonMap(int maxCol,int maxRow){
+	
+	public DungeonMap(int maxCol, int maxRow) {
 		this.maxRow = maxRow;
 		this.maxCol = maxCol;
 		rooms = new Room[maxRow][maxCol];
 	}
 
-	public int getMaxRow(){
+	public int getMaxRow() {
 		return maxRow;
 	}
 
-	public int getMaxCol(){
+	public int getMaxCol() {
 		return maxCol;
 	}
 
-	public boolean isRoom(MapPosition p){
+	public boolean isRoom(MapPosition p) {
 		boolean retCode = true;
-		if(isOutOfBounds(p) || rooms[p.getY()][p.getX()]==null)
+		if(isOutOfBounds(p) || rooms[p.getY()][p.getX()]==null) {
 			retCode = false;
-		return retCode;
-	}
-	public boolean isRoomInDir(MapPosition p,DIR direction){
-		boolean retCode = true;
-		MapPosition mp;
-		switch(direction){
-		case NORTH:
-			mp=p.translate(DIR.NORTH);
-			break;
-		case SOUTH:
-			mp=p.translate(DIR.SOUTH);
-			break;
-		case EAST:
-			mp=p.translate(DIR.EAST);
-			break;
-		case WEST:
-			mp=p.translate(DIR.WEST);
-			break;
-		default:
-			mp=p;
 		}
-		if(isOutOfBounds(mp) || rooms[mp.getY()][mp.getX()]==null)
-			retCode = false;
 		return retCode;
 	}
 	
-	public void addRoom(Room room, MapPosition position){
-		if(isOutOfBounds(position))
+	public boolean isRoomInDir(MapPosition p, DIR direction) {
+		boolean retCode = true;
+		MapPosition mp;
+		switch(direction) {
+		case NORTH:
+			mp = p.translate(DIR.NORTH);
+			break;
+		case SOUTH:
+			mp = p.translate(DIR.SOUTH);
+			break;
+		case EAST:
+			mp = p.translate(DIR.EAST);
+			break;
+		case WEST:
+			mp = p.translate(DIR.WEST);
+			break;
+		default:
+			mp = p;
+		}
+		if(isOutOfBounds(mp) || rooms[mp.getY()][mp.getX()] == null) {
+			retCode = false;
+		}
+		return retCode;
+	}
+	
+	public void addRoom(Room room, MapPosition position) {
+		if(isOutOfBounds(position)){
 			throw new IllegalArgumentException("The position ("
 												+ Integer.toString(position.getX())
 												+ ", "
 												+ Integer.toString(position.getY())
 												+") is out of bounds of the map. Room not added to DungeonMap.");
-		else if(rooms[position.getY()][position.getX()] != null)
+		}
+		else if(rooms[position.getY()][position.getX()] != null) {
 			throw new IllegalArgumentException("The position ("
 												+ Integer.toString(position.getX())
 												+ ", "
 												+ Integer.toString(position.getY())
-												+") already contains a room. Room not added to DungeonMap.");
-		rooms[position.getY()][position.getX()]=room;
+												+ ") already contains a room. Room not added to DungeonMap.");
+		}
+		rooms[position.getY()][position.getX()] = room;
 	}
 	
-	public Room getRoom(MapPosition pos){
-		if(isOutOfBounds(pos)){
+	public Room getRoom(MapPosition pos) {
+		if(isOutOfBounds(pos)) {
 			System.out.println("e:DungeonMap.getRoom():out of bounds of map");
 			return null;
 		}
