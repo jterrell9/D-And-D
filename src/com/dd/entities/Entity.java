f3d730e0189263da6ab0d1becf598bafcc4dd26c
Jack Terrell <trifeowns@gmail.com>
2017-03-22 03:17:50 -0400

    style standard, cleaned up some methods.

--- src/com/dd/entities/Entity.java
+++ src/com/dd/entities/Entity.java
@@ -6,53 +6,69 @@ public class Entity{
 	
 	protected String name;
 	protected Stats stats;
-	protected boolean isAlive = true;
+	protected boolean isAlive;
 	
 	public Entity(String name, int health, int maxHealth, int attack, int defense) {
-		this.name = name;
-		stats = new Stats(health,maxHealth,attack,defense);
+		setName(name);
+		setStats(new Stats(health, maxHealth, attack, defense));
 		isAlive = true;
 	}
-	public Entity(String name,Stats stats){
-		this.name = name;
-		this.stats = stats;
+	
+	public Entity(String name, Stats stats){
+		setName(name);
+		setStats(stats);
 		isAlive = true;
 	}
+	
 	public Entity(String name){
-		this.name = name;
-		this.stats = new Stats();
+		setName(name);
+		setStats(new Stats());
 		isAlive = true;
 	}
+	
 	public Entity(){
-		this.name = "player";
-		stats = new Stats();
+		setName("player");
+		setStats(new Stats());
 		isAlive = true;
 	}
+	
 	public boolean survives(){
 		if(stats.getHealth() <= 0){
-			stats.setHealth(0);
-			isAlive = false;
+			die();
 			return false;
 		}
 		return true;
 	}
+	
 	public void die(){
 		stats.setHealth(0);
 		isAlive = false;
 	}
+	
 	public void takeDamage(int damage){
 		stats.setHealth(stats.getHealth() - damage);
 		if(!survives()){
-			System.out.println("You just killed a " + name + "!");
+			System.out.println(getName() + "died!");
 		}
 	}
+	
 	public void attack(Entity entity){
 		entity.takeDamage(stats.getAttack());
 	}
+	
 	public String getName(){
 		return name;
 	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
 	public Stats getStats(){
 		return stats;
+	}
+	
+	public void setStats(Stats stats){
+		this.stats = stats;
 	}
 }
