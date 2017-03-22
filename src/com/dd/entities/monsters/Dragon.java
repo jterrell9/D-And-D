d7806c5149438f21a90a75bea6a445234880df95
meracoid <jcreech3@student.gsu.edu>
2017-03-22 18:35:22 -0400

    Added dragon description and stats

--- src/com/dd/entities/monsters/Dragon.java
+++ src/com/dd/entities/monsters/Dragon.java
@@ -1,29 +1,51 @@
 package com.dd.entities.monsters;
 
+import com.dd.entities.Entity;
 import com.dd.entities.Monster;
 import com.dd.entities.Player;
 
+import java.util.Random;
+
 public class Dragon extends Monster{
+
+    private String dragColor;
+    private boolean breathAttack;
 
     //Constructor used when Dragon is created for specific rooms when rooms are generated
     public Dragon (String name, int health, int attack, int defense){
         //set stats, set alive to true, set fight to false
     	super(name,health,attack,defense);
+        initDescription();
     }
 
-    //used when dragon attacks the player
-    //pre: if(isFight && isAlive)
-    //post: p1.getStats().getHealth() != 0
-    public void attack(Player p1){
-        //decides on which attack to do, either breath attack or claws, and deal damage
-        //to player
-    }
-
-    //used when the dragon takes damage
-    //pre: if(alive && fight)
-    //post: if(stats.getHealth() == 0) => die()
-    public void takeDamage(int damage){
-        //logic for taking damage
+    public void initDescription(){
+        Random random = new Random();
+        int color = random.nextInt(8) + 1;
+        switch (color) {
+            case 1: dragColor = "red";
+                break;
+            case 2: dragColor = "blue";
+                break;
+            case 3: dragColor = "black";
+                break;
+            case 4: dragColor = "green";
+                break;
+            case 5: dragColor = "white";
+                break;
+            case 6: dragColor = "bronze";
+                break;
+            case 7: dragColor = "brass";
+                break;
+            case 8: dragColor = "gold";
+                break;
+            case 9: dragColor = "silver";
+                break;
+        }
+        String desc = "As you enter the large room, the first thing that catches your eye is jewels, mostly " + dragColor
+                + ". As you walk around the abnormally large room, you hear a faint hum behind you. As you turn around, "
+                + "a giant " + dragColor + " scaled dragon faces you, nostrils flared. \"Fool! You think you could steal"
+                + "from " + getName() + " the " + dragColor + " dragon? For this, you shall die!\"";
+        setDescription(desc);
     }
 
     // used when dragon health reaches 0
@@ -31,7 +53,24 @@ public class Dragon extends Monster{
     // pre: if(alive)
     // post: fight = false
     // post: alive = false
-    public void die(){
+    public void takeDamage(int damage){
         //set health to 0 and other logic to ensure the battle is over
+        stats.setHealth(stats.getHealth() - damage);
+        if(!survives()){
+            System.out.println("You just killed " + name + " the "+ dragColor +" dragon!");
+        }
+    }
+
+    public void attack(Entity entity){
+        Random random = new Random;
+        if(breathAttack){
+            entity.takeDamage(5);
+            breathAttack = false;
+        }else{
+            if(random.nextInt(5) + 1 == 6){
+                breathAttack = true;
+            }
+        }
+        entity.takeDamage(stats.getAttack());
     }
 }
