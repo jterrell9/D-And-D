17cf35bfaf9c574d6da8d60bb4f08f14695792a4
Jack Terrell <trifeowns@gmail.com>
2017-03-22 02:48:01 -0400

    Style Compliance

--- src/com/dd/tester/Tester.java
+++ src/com/dd/tester/Tester.java
@@ -6,37 +6,28 @@ import java.util.Scanner;
 
 import com.dd.GameRunner;
 import com.dd.GameState;
-import com.dd.entities.Monster;
 import com.dd.entities.Player;
-import com.dd.entities.monsters.Dragon;
-import com.dd.items.Armour;
-import com.dd.items.Artifact;
-import com.dd.items.Item;
-import com.dd.items.OneHandedWeapon;
-import com.dd.items.Potion;
-import com.dd.items.Shield;
-import com.dd.items.TwoHandedWeapon;
-import com.dd.items.Weapon;
+import com.dd.entities.monsters.*;
+import com.dd.items.*;
 import com.dd.levels.DungeonMap;
 import com.dd.levels.MapPosition;
 import com.dd.levels.Room;
 
 public class Tester {
 	
-	private static String cmd=new String();
-	private static String opt=new String();
-	private static String[] opts=new String[10];
+	private static String cmd = new String();
+	private static String opt = new String();
+	private static String[] opts = new String[10];
 	private static int optNum;
 	
 	public static void go() throws FileNotFoundException{
-		
 		mainMenu();
 		printStats();
 		printMap();
 		System.out.println("*Type help for a list of commands*");
 		cmdLoop();
-	
 	}
+	
 	public static void cmdLoop() throws FileNotFoundException {
 		while(true){
 			enterCommand();
@@ -45,35 +36,39 @@ public class Tester {
 			printMap();
 		}
 	}
+	
 	public static void mainMenu() throws FileNotFoundException{
-		printLnTitle('*',"Main Menu",40);
+		printLnTitle('*', "Main Menu", 40);
 		System.out.println("Please select from the following:"
 				+"\n1: New Game"
 				+"\n2: Resume Game"
 				+"\n3: Quit");
 		System.out.print("Enter Selection >>");
 		try{
-			Scanner scanInt=new Scanner(System.in);
-			int selection=scanInt.nextInt();
+			Scanner scanInt = new Scanner(System.in);
+			int selection = scanInt.nextInt();
 			String name;
-			if(selection==1){		//new game
-				Scanner scanName=new Scanner(System.in);
+			if(selection == 1){		//new game
+				Scanner scanName = new Scanner(System.in);
 				System.out.print("Enter Player's Name: ");
 				name=scanName.next();
-				GameState game=new GameState(name,new Player(name),new DungeonMap(5,5));
+				GameState game = new GameState(name, new Player(name), new DungeonMap(5,5));
 				GameRunner.registerGameState(game);
 				GameRunner.setActiveGameState(game);
 				populate5x5(); //adds rooms to map;
-			}else if(selection==2){		//load game
-				Scanner scanName=new Scanner(System.in);
+			}
+			else if(selection == 2){		//load game *NOT FUNCTIONING
+				Scanner scanName = new Scanner(System.in);
 				System.out.print("Enter Player's Name: ");
-				name=scanName.nextLine();
+				name = scanName.nextLine();
 				//game.setMap(loadMap(name));
 				//game.addActivePlayer(loadPlayer(name));
-			}else if(selection==3){		//quit
+			}
+			else if(selection == 3){		//quit
 				System.out.println("Thank you for playing! GoodBye!");
 				System.exit(0);
-			}else{
+			}
+			else{
 				System.out.println("\n!e:Invalid entry, please try again.\n");
 				mainMenu();
 			}
@@ -89,6 +84,7 @@ public class Tester {
 	}
 	
 	public static void mapCmd() throws FileNotFoundException {
+		
 		Item item;
 		
 		switch(cmd){
@@ -111,37 +107,45 @@ public class Tester {
 			return;
 			
 		case "move":
-			if(opts[0]!=null){
+			if(opts[0] != null){
 				switch(opts[0]){
 				case "north": 
-					if(getRunnerMap().isRoomInDir(getRunnerPosition(),DIR.NORTH)){
+					if(getRunnerMap().isRoomInDir(getRunnerPosition(), DIR.NORTH)){
 						getRunnerPosition().moveNorth();
-						System.out.println(getRunnerPlayer().getName()+" has moved North");
-					}else{
+						System.out.println(getRunnerPlayer().getName()
+								+ " has moved North");
+					}
+					else{
 						System.out.println("No Door in that Direction!");
 					}
 					return;
 				case "south": 
-					if(getRunnerMap().isRoomInDir(getRunnerPosition(),DIR.SOUTH)){
+					if(getRunnerMap().isRoomInDir(getRunnerPosition(), DIR.SOUTH)){
 						getRunnerPosition().moveSouth();
-						System.out.println(getRunnerPlayer().getName()+" has moved South");
-					}else{
+						System.out.println(getRunnerPlayer().getName()
+								+ " has moved South");
+					}
+					else{
 						System.out.println("No Door in that Direction!");
 					}
 					return;
 				case "east": 
-					if(getRunnerMap().isRoomInDir(getRunnerPosition(),DIR.EAST)){
+					if(getRunnerMap().isRoomInDir(getRunnerPosition(), DIR.EAST)){
 						getRunnerPosition().moveEast();
-						System.out.println(getRunnerPlayer().getName()+" has moved East");
-					}else{
+						System.out.println(getRunnerPlayer().getName()
+								+ " has moved East");
+					}
+					else{
 						System.out.println("No Door in that Direction!");
 					}
 					return;
 				case "west": 
-					if(getRunnerMap().isRoomInDir(getRunnerPosition(),DIR.WEST)){
+					if(getRunnerMap().isRoomInDir(getRunnerPosition(), DIR.WEST)){
 						getRunnerPosition().moveWest();
-						System.out.println(getRunnerPlayer().getName()+" has moved West");
-					}else{
+						System.out.println(getRunnerPlayer().getName()
+								+ " has moved West");
+					}
+					else{
 						System.out.println("No Door in that Direction!");
 					}
 					return;
@@ -153,7 +157,7 @@ public class Tester {
 		case "examine":
 			switch(opts[0]){
 			case "room":
-				Tester.printLnTitle('~',"Examine Room",40);
+				Tester.printLnTitle('~',"Examine Room", 40);
 				if(getRunnerMap().isRoom(getRunnerPosition())){
 					if(!getRunnerRoom().getMosterList().isEmpty()){
 						System.out.println("This Room Has A Monster:");
@@ -169,11 +173,13 @@ public class Tester {
 							i++;
 						}
 						return;
-					}else{
+					}
+					else{
 						System.out.println("This room is empty.");
 						return;
 					}
-				}else{
+				}
+				else{
 					System.out.println("ERROR no room here!");
 				}
 				return;
@@ -183,7 +189,8 @@ public class Tester {
 				
 				for(String monsterName:getRunnerRoom().getMosterList()){
 					System.out.println(monsterName
-							+"\nHealth:\t\t"+getRunnerRoom().getMonster(monsterName).getStats().getHealth()
+							+"\nHealth:\t\t"
+							+getRunnerRoom().getMonster(monsterName).getStats().getHealth()
 							+"\nAttack/Defense:\t"
 							+getRunnerRoom().getMonster(monsterName).getStats().getAttack()
 							+"/"+getRunnerRoom().getMonster(monsterName).getStats().getDefense());
@@ -294,6 +301,7 @@ public class Tester {
 				return;
 			}
 			return;
+		*/
 		case "help":
 			Tester.printLnTitle('*',"Commands",40);
 			System.out.println("'quit' to quit\n"
@@ -308,30 +316,31 @@ public class Tester {
 					+"'attack'");
 					
 			return;
-		*/
+		
 		default:
-			System.out.println("This command not recognized\nType help for a list of commands");
+			System.out.println("This command not recognized\n"
+					+ "Type help for a list of commands");
 			break;
 		}
 	}
 	
 	public static void promptParse(){
 		Scanner user=new Scanner(System.in);
-		System.out.print(getRunnerPlayer().getName()+">> ");
+		System.out.print(getRunnerPlayer().getName() + ">> ");
 		String userInput=user.nextLine();
 		String[] input=userInput.toLowerCase().split(" ");
 		cmd=input[0];
-		if(input.length>1 && input.length<11){
-			for(int i=1;i<input.length;i++){
-				opt+=input[i]+" ";
-				opts[i-1]=input[i];
+		if(input.length > 1 && input.length < 11){
+			for(int i = 1; i < input.length; i++){
+				opt += input[i] + " ";
+				opts[i-1] = input[i];
 				if(isInteger(input[i])){
-					optNum=Integer.parseInt(input[i]);
+					optNum = Integer.parseInt(input[i]);
 				}
-				
 			}
 		}
 	}
+	
 	public static boolean isInteger(String str) {
 		try {
 			Integer.parseInt(str);
@@ -340,35 +349,38 @@ public class Tester {
 			return false;
 		}
 	}
+	
 	public static void printStats(){
-		printLnTitle('~',getRunnerPlayer().getName()+"'s Stats Board",40);
+		printLnTitle('~', getRunnerPlayer().getName() + "'s Stats Board", 40);
 		System.out.println(getRunnerPlayer().statboardToString());
 	}
+	
 	public static void printMap(){
-		printLnTitle('-',"Map",40);
-		MapPosition playerPos=getRunnerPlayer().getPostion();
-		for(int y=0;y<getRunnerMap().getMaxRow();y++){
-			for(int x=0;x<getRunnerMap().getMaxCol();x++){
-				if(x==0)
+		printLnTitle('-', "Map", 40);
+		MapPosition playerPos = getRunnerPlayer().getPostion();
+		for(int y = 0; y < getRunnerMap().getMaxRow(); y++){
+			for(int x = 0; x < getRunnerMap().getMaxCol(); x++){
+				if(x == 0)
 					System.out.print("\t|");
-				if(playerPos.getX()==x && playerPos.getY()==y)
+				if(playerPos.getX() == x && playerPos.getY() == y)
 					System.out.print("#");
-				else if(getRunnerMap().isRoom(new MapPosition(x,y)))
+				else if(getRunnerMap().isRoom(new MapPosition(x, y)))
 					System.out.print("X");
 				else
 					System.out.print(" ");
 			}
 			System.out.print("|\n");
 		}
-		printLnTitle('-',"",40);
+		printLnTitle('-', "", 40);
 	}
-	public static void printLnTitle(char c,String str,int width){
-		int strLength=str.length();
-		int startIndex=(width/2)-(strLength/2);
-		for(int i=0;i<=width;i++){
-			if(i==startIndex){
+	
+	public static void printLnTitle(char c, String str, int width){
+		int strLength = str.length();
+		int startIndex = (width / 2) - (strLength / 2);
+		for(int i = 0; i <= width; i++){
+			if(i == startIndex){
 				System.out.print(str);
-				i+=strLength;
+				i += strLength;
 			}else{
 				System.out.print(c);
 			}
@@ -377,107 +389,52 @@ public class Tester {
 	}
 	
 	public static void populate5x5(){
-		OneHandedWeapon sword=new OneHandedWeapon("Sword",2);
-		Shield shield=new Shield("shield",4);
-		Artifact ring=new Artifact("Ring",0,5,1,1);
-		Potion potion=new Potion("Health Elixer",10);
-		Armour breastPlate=new Armour("Breast Plate",2);
+		OneHandedWeapon sword = new OneHandedWeapon("Sword", 2);
+		Shield shield = new Shield("shield", 4);
+		Artifact ring = new Artifact("Ring", 0, 5, 1, 1);
+		Potion potion = new Potion("Health Elixer", 10);
+		Armour breastPlate = new Armour("Breast Plate", 2);
 		
-		Dragon dragon=new Dragon("Dragon",10,5,5);
+		Dragon dragon = new Dragon("Dragon", 10, 5, 5);
 		
-		
-		MapPosition buildPos=new MapPosition();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		MapPosition buildPos = new MapPosition();
+		getRunnerMap().addRoom(new Room(), buildPos);
 		getRunnerMap().getRoom(buildPos).addItem(sword);
 		getRunnerMap().getRoom(buildPos).addItem(shield);
 		buildPos.moveEast();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		getRunnerMap().getRoom(buildPos).addItem(breastPlate);
 		getRunnerMap().getRoom(buildPos).addItem(ring);
 		getRunnerMap().getRoom(buildPos).addItem(potion);
 		getRunnerMap().getRoom(buildPos).addMonster(dragon);
 		buildPos.moveEast();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveSouth();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveSouth();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveEast();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveSouth();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveSouth();
-		getRunnerMap().addRoom(new Room(),buildPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 		buildPos.moveEast();
-		getRunnerMap().addRoom(new Room(),buildPos);
-		
-		
-	}
-	
-	public static DungeonMap old5x5(){
-		DungeonMap maze=new DungeonMap(5,5);
-		MapPosition buildPosition=new MapPosition();
-		
-		Room room00=new Room();
-		OneHandedWeapon sword=new OneHandedWeapon("Sword",2);
-		room00.addItem(sword);
-		Shield shield=new Shield("shield",4);
-		room00.addItem(shield);
-		Artifact ring=new Artifact("Ring",0,5,1,1);
-		room00.addItem(ring);
-		Potion potion=new Potion("Health Elixer",10);
-		room00.addItem(potion);
-		maze.addRoom(room00, buildPosition);
-		
-		Room room01=new Room();
-		Armour breastPlate=new Armour("Breast Plate",2);
-		room01.addItem(breastPlate);
-		buildPosition.moveSouth();
-		maze.addRoom(room01, buildPosition);
-		
-		Room room11=new Room();
-		Artifact ring2=new Artifact("Ring",0,5,1,1);
-		room11.addItem(ring2);
-		room11.addItem(potion);
-		buildPosition.moveEast();
-		maze.addRoom(room11, buildPosition);
-		addRoom(buildPosition,DIR.EAST);
-		addRoom(buildPosition,DIR.SOUTH);
-		addRoom(buildPosition,DIR.SOUTH);
-		addRoom(buildPosition,DIR.SOUTH);
-		addRoom(buildPosition,DIR.EAST);
-		addRoom(buildPosition,DIR.EAST);
-		
-		return maze;
-	}
-	public static void addRoom(MapPosition startPos,DIR dir){
-		Room room=new Room();
-		switch(dir){
-		case NORTH:
-			startPos.moveNorth();
-			break;
-		case SOUTH:
-			startPos.moveSouth();
-			break;
-		case EAST:
-			startPos.moveEast();
-			break;
-		case WEST:
-			startPos.moveWest();
-			break;
-		}
-		getRunnerMap().addRoom(room, startPos);
+		getRunnerMap().addRoom(new Room(), buildPos);
 	}
 
 	public static Player getRunnerPlayer(){
 		return GameRunner.getActiveGameState().getActivePlayer();
 	}
+	
 	public static DungeonMap getRunnerMap(){
 		return GameRunner.getActiveGameState().getMap();
 	}
+	
 	public static MapPosition getRunnerPosition(){
 		return getRunnerPlayer().getPostion();
 	}
+	
 	public static Room getRunnerRoom(){
 		return getRunnerMap().getRoom(getRunnerPosition());
 	}
