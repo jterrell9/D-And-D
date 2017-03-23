f633cce95d43d4a0300c0b6e64b2a73f0ccc6f44
meracoid <jcreech3@student.gsu.edu>
2017-03-23 18:39:03 -0400

    Other other other other merge conflict issues

--- src/com/dd/entities/Monster.java
+++ src/com/dd/entities/Monster.java
@@ -9,23 +9,7 @@ public class Monster extends Entity {
 	public Monster(String name, int health, int attack, int defense) {
 		super(name, health, health, attack, defense);
 	}
-
-	public int attackDamage(){
-		return stats.getAttack();
-	}
-	public void examine(){
-		System.out.println(name
-				+"\nHealth:\t\t"+stats.getHealth()
-				+"\nAttack/Defense:\t"+stats.getAttack()+"/"+stats.getDefense());
-	}
-
-	@Override
-	public String toString(){
-		return name
-				+"\nHealth:\t\t"+stats.getHealth()+"/"+stats.getMaxHealth()
-				+"\nAttack/Defense:\t"+stats.getAttack()+"/"+stats.getDefense();
-	}
-
+	
 	public String getDescription(){
 		return description;
 	}
