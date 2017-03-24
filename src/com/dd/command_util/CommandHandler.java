bd504b3d439ef2f7efec505aec91679d32c3460e
Jack Terrell <trifeowns@gmail.com>
2017-03-24 01:12:13 -0400

    add get gamestate methods

--- src/com/dd/command_util/CommandHandler.java
+++ src/com/dd/command_util/CommandHandler.java
@@ -1,5 +1,11 @@
 package com.dd.command_util;
 
+import com.dd.GameRunner;
+import com.dd.entities.Player;
+import com.dd.levels.DungeonMap;
+import com.dd.levels.MapPosition;
+import com.dd.levels.Room;
+
 public abstract class CommandHandler {
     public abstract void handleCommand(String[] args, CommandOutputLog outputLog) throws CommandHandlerException;
 
@@ -8,4 +14,19 @@ public abstract class CommandHandler {
             super(message);
         }
     }
+    public static Player getRunnerPlayer(){
+		return GameRunner.getActiveGameState().getActivePlayer();
+	}
+	
+	public static DungeonMap getRunnerMap(){
+		return GameRunner.getActiveGameState().getMap();
+	}
+	
+	public static MapPosition getRunnerPosition(){
+		return getRunnerPlayer().getPostion();
+	}
+	
+	public static Room getRunnerRoom(){
+		return getRunnerMap().getRoom(getRunnerPosition());
+	}
 }
