18905bf5ce50d91f942f3b203c741c830154cba8
Jack Terrell <trifeowns@gmail.com>
2017-03-24 02:23:23 -0400

    add toString methods, style conform for item class

--- src/com/dd/items/Item.java
+++ src/com/dd/items/Item.java
@@ -2,38 +2,64 @@ package com.dd.items;
 
 import com.dd.Stats;
 
-public class Item{
-	private String name;
-	private Stats StatModifyer;
+public class Item {
+	
+	protected String name;
+	protected Stats StatModifyer;
+	
 	public Item(String name) {
 		this.name=name;
 		StatModifyer=new Stats(0,0,0,0);
 	}
+	
 	public String getName() {
 		return name;
 	}
 	
-	public void setStatChange(int health,int maxHealth,int attack,int defense){
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setStatChange(int health, int maxHealth, int attack, int defense) {
 		StatModifyer=new Stats(health,maxHealth,attack,defense);
 	}
-	public Stats getStatChange(){
+	
+	public Stats getStatChange() { 
 		return StatModifyer;
 	}
-	public Stats getNegStatChange(){
-		int negHealth=-1*StatModifyer.getHealth();
-		int negMaxHealth=-1*StatModifyer.getMaxHealth();
-		int negAttack=-1*StatModifyer.getAttack();
-		int negDefense=-1*StatModifyer.getDefense();
-		Stats negStats=new Stats(negHealth,negMaxHealth,negAttack,negDefense);
+	
+	public Stats getNegStatChange() {
+		int negHealth = -1*StatModifyer.getHealth();
+		int negMaxHealth = -1*StatModifyer.getMaxHealth();
+		int negAttack = -1*StatModifyer.getAttack();
+		int negDefense = -1*StatModifyer.getDefense();
+		Stats negStats = new Stats(negHealth, negMaxHealth, negAttack, negDefense);
 		return negStats;
 	}
 	
-	public String typeToString(){
+	public String statModToString() {
+		StringBuilder modStr=new StringBuilder();
+		if(getStatChange().getHealth()>0){
+			modStr.append("[+" + getStatChange().getHealth() + " Health]");
+		}
+		if(getStatChange().getMaxHealth()>0){
+			modStr.append("[+" + getStatChange().getMaxHealth() + " Max Health]");
+		}
+		if(getStatChange().getAttack()>0) {
+			modStr.append("[+" + getStatChange().getAttack() + " Attack]");
+		}
+		if(getStatChange().getDefense()>0){
+			modStr.append("[+" + getStatChange().getDefense() + " Defense]");
+		}
+		return modStr.toString();
+	}
+	
+	public String typeToString() {
 		return getClass().toString().substring(19);
 	}
 	
 	@Override
 	public String toString(){
-		return name;
+		return "("+typeToString()+") " + getName() + " " + statModToString();
 	}
 }
