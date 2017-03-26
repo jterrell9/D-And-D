04060446f8f7d0352cf2613ec65af928f2857b65
Jack Terrell <trifeowns@gmail.com>
2017-03-26 14:03:30 -0400

    QuitCommand - Add quit message

--- src/com/dd/command_util/command/QuitCommand.java
+++ src/com/dd/command_util/command/QuitCommand.java
@@ -2,12 +2,14 @@ package com.dd.command_util.command;
 
 import com.dd.command_util.CommandHandler;
 import com.dd.command_util.CommandOutputLog;
+import com.dd.tester.Tester;
 
 public class QuitCommand extends CommandHandler {
     public QuitCommand(){}
 
     @Override
     public void handleCommand(String[] args, CommandOutputLog outputLog){
+    	Tester.printToLog("Thank you for playing! Goodbye.");
     	System.exit(0);
     }
 }
