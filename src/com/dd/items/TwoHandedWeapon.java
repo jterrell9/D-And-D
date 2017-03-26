9b2b874b19053cc5e598c1ecae5bb608916f37f6
Jack Terrell <trifeowns@gmail.com>
2017-03-26 13:19:03 -0400

    TwoHandedWeapon class - override TypeToString() method

--- src/com/dd/items/TwoHandedWeapon.java
+++ src/com/dd/items/TwoHandedWeapon.java
@@ -4,4 +4,9 @@ public class TwoHandedWeapon extends Weapon {
     public TwoHandedWeapon(String name, int attackGain){
         super(name, attackGain);
     }
+    
+    @Override
+    public String typeToString() {
+		return "Two Handed Weapon";
+	}
 }
