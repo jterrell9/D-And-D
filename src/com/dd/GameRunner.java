3a4b99e05455906c7e3be1d164cc8e49944bae03
wnolan <wnolan1@student.gsu.edu>
2017-03-27 14:26:15 -0400

    Moved main method
    - Moved logic from MainController into GameRunner's main method.

--- src/com/dd/GameRunner.java
+++ src/com/dd/GameRunner.java
@@ -3,12 +3,16 @@ package com.dd;
 import com.dd.GameState;
 import com.dd.gamescene_util.GameScene;
 import com.dd.gamescene_util.gamescene.*;
+
+import java.io.IOException;
 import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -28,8 +32,26 @@ public class GameRunner extends Application {
     public void start(Stage primaryStage) {
         stage = primaryStage;
 
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/dd/fxml/MainMenu.fxml"));
+			Scene scene = new Scene(loader.load());
+			
+			primaryStage.setTitle("D&D");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch (IllegalStateException e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+        
+		/*
         screenWidth = 1920;
-        screenHeight = 1080;
+        screenHeight = 1080;        
 
         GameScene mainMenuScene =  new MainMenuScene(new StackPane(),
                                                         screenWidth,
@@ -62,7 +84,9 @@ public class GameRunner extends Application {
         addGameScene("RunningGameScene", runningGameScene);
 
         setActiveGameScene("MainMenuScene", null);
+        
         primaryStage.show();
+        */
     }
 
     private static void addGameScene(String name, GameScene gameScene) {
