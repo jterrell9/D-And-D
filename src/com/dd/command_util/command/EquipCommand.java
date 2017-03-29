74a2e9465f50a4137ab7e798f5d706732c51fe90
Jack Terrell <trifeowns@gmail.com>
2017-03-29 16:50:52 -0400

    add logic to EquipCommand class

--- src/com/dd/command_util/command/EquipCommand.java
+++ src/com/dd/command_util/command/EquipCommand.java
@@ -2,12 +2,32 @@ package com.dd.command_util.command;
 
 import com.dd.command_util.CommandHandler;
 import com.dd.command_util.CommandOutputLog;
+import com.dd.entities.Player.EquipmentException;
+import com.dd.entities.Player.InventoryException;
+import com.dd.items.*;
+import com.dd.tester.Console;
 
 public class EquipCommand extends CommandHandler {
     public EquipCommand() {}
 
     @Override
-    public void handleCommand(String[] args){
-
+    public void handleCommand(String[] args) {
+    	Item item = currRoom().getItem(args[0]);
+    	if(item != null){
+    		try{
+    			player().equip(item);
+    			output.append(player().getName() + " has equipped " + item.getName());
+    		}
+    		catch(EquipmentException ee) {
+    			output.append(ee.toString());
+    		}
+    		catch(InventoryException ie) {
+    			output.append(ie.toString());
+    		}
+    	}
+    	else {
+    		output.append("The item \"" + args[0] + "\" is not in this room.");
+    	}
+    	Console.updateScreen(output.toString());
     }
 }
