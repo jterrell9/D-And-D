1e250783d3dee6002ab5292c3294702b7ecf2afa
Jack Terrell <trifeowns@gmail.com>
2017-03-30 11:01:54 -0400

    CommandParser - registered all commands, even if they are blank.

--- src/com/dd/command_util/CommandParser.java
+++ src/com/dd/command_util/CommandParser.java
@@ -3,6 +3,7 @@ package com.dd.command_util;
 import com.dd.command_util.CommandHandler;
 import com.dd.command_util.CommandHandler.CommandHandlerException;
 import com.dd.command_util.command.*;
+import com.dd.tester.Console;
 
 import javafx.scene.control.TextInputControl;
 
@@ -15,16 +16,20 @@ public class CommandParser {
     private Map<String, CommandHandler> commandMap = new HashMap<String, CommandHandler>();
     private CommandOutputLog outputLog;
     private String command = new String();
-	private String[] arguments = {"~!", null};
+	private String[] arguments = {null, null};
 
     public CommandParser(){
-    	registerCommand("help", new HelpCommand());
-		registerCommand("menu", new MenuCommand());
+    	registerCommand("menu", new MenuCommand());
 		registerCommand("quit", new QuitCommand());
 		registerCommand("move", new MoveCommand());
 		registerCommand("examine", new ExamineCommand());
 		registerCommand("save", new SaveCommand());
+		registerCommand("drop", new DropCommand());
+		registerCommand("attack", new AttackCommand());
 		registerCommand("equip", new EquipCommand());
+		registerCommand("help", new HelpCommand());
+		registerCommand("pickup", new PickupCommand());
+		registerCommand("use", new UseCommand());
     }
     
     public CommandParser(CommandOutputLog outputLog) {
@@ -32,25 +37,45 @@ public class CommandParser {
     }
     
     public void parse(String userInput) throws FileNotFoundException, CommandHandlerException {
-		CommandParser parser = new CommandParser();
-		String[] input = userInput.split(" ");
-		command = input[0].toLowerCase();
-		if(input.length > 1) {
-			arguments[0] = userInput.substring(command.length() + 1);
-			arguments[1] = null;
+		if(userInput != null) {
+			String[] input = userInput.split(" ");
+			command = input[0].toLowerCase();
+			if(input.length > 1) {
+				arguments[0] = userInput.substring(command.length() + 1);
+				arguments[1] = input[input.length-1];
+			}
+			CommandHandler handler = commandMap.get(command);
+			if(handler == null) {
+				Console.updateScreen("The command \""
+						+ command
+						+ "\" is invalid.");
+				return;
+			}
+	        try{
+	            handler.handleCommand(arguments);
+	        }
+	        catch(CommandHandler.CommandHandlerException E){
+	            handler.output.append(E.toString());
+	        }
+	        catch(IllegalArgumentException IAE){
+	            handler.output.append(IAE.toString());
+	        }
 		}
-		CommandHandler handler = commandMap.get(command);
-		if(handler == null)
-            throw new IllegalArgumentException("The command \""
-                                                    + command
-                                                    + "\" is invalid.");
-        try{
-            handler.handleCommand(arguments);
-        }
-        catch(CommandHandler.CommandHandlerException e){
-            throw e;
-        }
 	}
+    
+    public void initCommands() {
+    	registerCommand("menu", new MenuCommand());
+		registerCommand("quit", new QuitCommand());
+		registerCommand("move", new MoveCommand());
+		registerCommand("examine", new ExamineCommand());
+		registerCommand("save", new SaveCommand());
+		registerCommand("drop", new DropCommand());
+		registerCommand("attack", new AttackCommand());
+		registerCommand("equip", new EquipCommand());
+		registerCommand("help", new HelpCommand());
+		registerCommand("pickup", new PickupCommand());
+		registerCommand("use", new UseCommand());
+    }
 
     public void registerCommand(String commandName, CommandHandler commandHandler) {
         if(commandMap.containsKey(commandName))
