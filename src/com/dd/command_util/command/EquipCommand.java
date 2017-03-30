7ffb75fe979c385a5dedc3d55f38b3cde875bcd8
Jack Terrell <trifeowns@gmail.com>
2017-03-30 11:32:10 -0400

    Equip Command - fixed removeItem problem

--- src/com/dd/command_util/command/EquipCommand.java
+++ src/com/dd/command_util/command/EquipCommand.java
@@ -17,7 +17,8 @@ public class EquipCommand extends CommandHandler {
     	if(item != null){
     		try{
     			player().equip(item);
-    			currRoom().removeItem(item);
+    			currRoom().removeItem(item.getName());
+    			
     			output.append(player().getName() + " has equipped " + item.getName() + "\n");
     		}
     		catch(EquipmentException ee) {
@@ -25,7 +26,9 @@ public class EquipCommand extends CommandHandler {
     		}
     		catch(InventoryException ie) {
     			output.append(ie.toString());
-    		}
+    		} catch (UnknownItemException UIE) {
+				output.append(UIE.toString());
+			}
     	}
     	else {
     		output.append("The item \"" + args[0] + "\" is not in this room.\n");
