e6a2b66cc59e90b83ca455f29a19eb7ffb190720
Jack Terrell <trifeowns@gmail.com>
2017-04-22 06:46:53 -0400

    remove type enums

--- src/com/dd/dataTypes/enums/ItemType.java
+++
@@ -1,5 +0,0 @@
-package com.dd.dataTypes.enums;
-
-public enum ItemType {
-	ARTIFACT, MAGICAL, ONEHANDEDWEAPON, POTION, SHIELD, SUIT, TWOHANDEDWEAPON, NONE;
-}
--- src/com/dd/dataTypes/enums/PlayerType.java
+++
@@ -1,5 +0,0 @@
-package com.dd.dataTypes.enums;
-
-public enum PlayerType {
-	WIZARD, FIGHTER, NONE;
-}
