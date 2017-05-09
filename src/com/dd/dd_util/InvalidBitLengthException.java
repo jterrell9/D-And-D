365f5685fbaa837a58836aa65e19c118daf3ae9a
Jack Terrell <trifeowns@gmail.com>
2017-05-08 22:44:24 -0400

    remove network classes

--- src/com/dd/dd_util/InvalidBitLengthException.java
+++
@@ -1,7 +0,0 @@
-package com.dd.dd_util;
-
-public class InvalidBitLengthException extends RuntimeException {
-    public InvalidBitLengthException(String message){
-        super(message);
-    }
-}
