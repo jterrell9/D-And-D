a1131ec7aab8cf265590a0fb235c739eb5f4d4ca
Jack Terrell <trifeowns@gmail.com>
2017-05-09 00:02:28 -0400

    pickupCommand - change access to gamestate objects

--- src/com/dd/command_util/command/PickupCommand.java
+++ src/com/dd/command_util/command/PickupCommand.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import com.dd.GameState;
 import com.dd.command_util.CommandHandler;
 import com.dd.command_util.CommandOutputLog;
-import com.dd.command_util.LocalCommandOutputLog;
 import com.dd.exceptions.*;
 import com.dd.items.*;
 
@@ -18,9 +17,8 @@ public class PickupCommand extends CommandHandler {
     @Override
     public void handleCommand(String commandName, String[] args, CommandOutputLog output) throws InvalidArgumentException {
     	setGlobalOutput(output);
-		updateState();
-    	if(dead){
-    		output.print(player.getTitle() + " is dead. ");
+    	if(isDead()){
+    		output.print(player().getTitle() + " is dead. ");
     		return;
     	}
     	if(args[0] == null) {
@@ -31,10 +29,10 @@ public class PickupCommand extends CommandHandler {
 		switch(args[0]) {
 		case "items":
 			ArrayList<String> equippedItemNames = new ArrayList<String>();
-			for(Item equippedItem : room.getItemMap().values()) {
+			for(Item equippedItem : room().getItemMap().values()) {
 				try {
-					Item item = room.getItem(equippedItem);
-					player.pickup(item);
+					Item item = room().getItem(equippedItem);
+					player().pickup(item);
 					equippedItemNames.add(equippedItem.getName());
 				}
 				catch(NullItemException | EquipmentException E) {
@@ -43,8 +41,8 @@ public class PickupCommand extends CommandHandler {
 			}
 			for(String itemName : equippedItemNames) {
 	    		try {
-	    			room.removeItem(itemName);
-	    			output.print(player.getTitle() + " has equipped " + itemName + ". ");
+	    			room().removeItem(itemName);
+	    			output.print(player().getTitle() + " has equipped " + itemName + ". ");
 	    		}
 	    		catch (NullItemException UIE) {
 	    			output.print(UIE.getMessage());
@@ -53,21 +51,21 @@ public class PickupCommand extends CommandHandler {
 			break;
 		default:
 			try {
-				Item item = room.getItem(args[0]);
-				player.pickup(item);
-				room.removeItem(item.getName());
-				output.print(player.getTitle() + " has equipped " + item.getTitle() + ". ");
+				Item item = room().getItem(args[0]);
+				player().pickup(item);
+				room().removeItem(item.getName());
+				output.print(player().getTitle() + " has equipped " + item.getTitle() + ". ");
 			}
 			catch(NullItemException | EquipmentException E) {
 				output.print(E.getMessage());
 				return;
 			}
 		}
-		if(!room.hasItems()) {
+		if(!room().hasItems()) {
 			output.print("This room now has no items. ");
 			return;
 		}
 		output.print("This room now contains the following items:\n");
-		output.print(room.examineItems());	
+		output.print(room().examineItems());	
     }
 }
